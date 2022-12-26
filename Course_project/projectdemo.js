952a6e6fbac7ce7d76f7d0686edf7b7b56704824
Weiguang0406 <weiguang0406@hotmail.com>
2022-12-25 22:57:11 -0500

    12/25

--- Course_project/projectdemo.js
+++ Course_project/projectdemo.js
@@ -30,3 +30,17 @@ function showSlides(n) {
   slides[slideIndex - 1].style.display = "block";
   dots[slideIndex - 1].className += " active";
 }
+
+const primaryNav = document.querySelector(".primary-navigation");
+const navToggle = document.querySelector(".mobile-nav-toggle");
+
+navToggle.addEventListener("click", () => {
+  const visibility = primaryNav.getAttribute("data-visible");
+  if (visibility === "false") {
+    primaryNav.setAttribute("data-visible", true);
+    navToggle.setAttribute(aria - expanded, "true");
+  } else if (visibility === "true") {
+    primaryNav.setAttribute("data-visible", false);
+    navToggle.setAttribute(aria - expanded, "false");
+  }
+});
